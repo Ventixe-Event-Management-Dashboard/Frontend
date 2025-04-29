46307ad66616b53c305534ba56a3a65b754ae6d7
Oliver <oliver.stolpe@hotmail.se>
2025-04-29 13:15:04 +0200

    Form validation added

--- Frontend/Controllers/AuthController.cs
+++ Frontend/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Frontend.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Frontend.Controllers
 {
@@ -10,10 +11,36 @@ namespace Frontend.Controllers
             return View();
         }
 
+        [HttpPost]
+        [Route("signup")]
+        public IActionResult SignUp(SignUpForm form)
+        {
+            if (!ModelState.IsValid)
+                return View(form);
+
+            // Här ska _authService.SignUp(form) callas och account ska skapas
+
+            //redirect till signin så länge, om alla fält är ifyllda
+            return RedirectToAction("SignIn");
+        }
+
         [Route("signin")]
         public IActionResult SignIn()
         {
             return View();
+        }
+
+        [Route("signin")]
+        [HttpPost]
+        public IActionResult SignIn(SignInForm form)
+        {
+            if (!ModelState.IsValid)
+                return View(form);
+
+            //här ska _authService.SignIn(form) callas kolla validera inloggning
+
+            //redirect till dashboard så länge
+            return RedirectToAction("Index", "Dashboard");
         }
     }
 }
