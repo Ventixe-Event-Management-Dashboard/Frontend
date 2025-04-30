fbc312eb21e26d288506f648cc2c7fcf7fbda0e6
Jonathan <jonathanfromsweden@gmail.com>
2025-04-30 22:35:21 +0200

    Implemented hambugermenu for mobileusers. This is a preliminary implementation, so changes may come in a distant future.

--- Frontend/wwwroot/js/site.js
+++ Frontend/wwwroot/js/site.js
@@ -1,1 +1,15 @@
-﻿
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.getElementById('hamburgerMenuButton');
+    const mobileMenu = document.getElementById('mobileMenu');
+    const closeMenu = document.getElementById('closeMenu');
+
+    if (hamburger && mobileMenu && closeMenu) {
+        hamburger.addEventListener('click', () => {
+            mobileMenu.classList.add('open');
+        });
+
+        closeMenu.addEventListener('click', () => {
+            mobileMenu.classList.remove('open');
+        });
+    }
+});
