2a3899f646bb18e9eeaa085bd4c2719d0b6ae955
Oliver <oliver.stolpe@hotmail.se>
2025-05-19 18:10:33 +0200

    Added signup, signin and signout functions

---
+++ AuthService/Services/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using Authentication.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Authentication.Services
+{
+    public interface IAuthService
+    {
+        Task<SignInResult> SignInAsync(string email, string password);
+        Task SignOutAsync();
+        Task<IdentityResult> SignUpAsync(AppUser user, string password);
+    }
+}
